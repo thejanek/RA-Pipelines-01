6d6ae7e1a1decf7ced755b52c9742adb9bee8d83
Lukasz Jankowski <Lukasz.Jankowski@rockwellautomation.com>
2020-09-08 16:36:03 +0200

    2020-09-08--04-36

--- Code/PowerShell/Triggers/Get-Env.ps1
+++ Code/PowerShell/Triggers/Get-Env.ps1
@@ -3,4 +3,20 @@ Write-Host "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 # Get-AzContext
 
 
+Start-Sleep -Seconds 180
+
 Get-ChildItem env: | ft Name,Value -AutoSize -Wrap
+
+
+
+
+
+Write-Host "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+
+
+Get-Location
+
+Write-host  "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+
+
+
